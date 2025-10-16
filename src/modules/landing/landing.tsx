7a3571f1b3521210fad6f17cce6eab37c1108e58
Geraldine Lavadenz <riomahabramromamateo@gmail.com>
2025-10-15 22:20:27 -0400

    `Added BookShelvesSection component to Landing page`

--- src/modules/landing/landing.tsx
+++ src/modules/landing/landing.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import HeroSection from './components/HeroSection';
 import BookGallerySection from './components/BookGallerySection';
+import BookShelvesSection from './components/BookShelvesSection';
 
 const Landing: React.FC = () => {
   return (
     <>
       <HeroSection />
       <BookGallerySection />
+      <BookShelvesSection />
     </>
   );
 };
