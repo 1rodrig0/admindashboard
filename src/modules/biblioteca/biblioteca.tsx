9db21a7742a14de4f8df665233cf10c3e237c6ef
Geraldine Lavadenz <riomahabramromamateo@gmail.com>
2025-10-29 20:17:54 -0400

    `Updated import statement in BookGallerySection.tsx from theme-provider.tsx to theme-provider.jsx`

---
+++ src/modules/biblioteca/biblioteca.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function biblioteca() {
+  return (
+    <div>
+        Historias de
+        <section className="BusquedaEtiquta"></section>
+        <section className='busqueda_libros'>
+            <div className='filtro'></div>
+            <div className='librosSection'></div>
+        </section>
+      
+    </div>
+  )
+}
+
+export default biblioteca
